17beb2bdb5536fbd6cfac430477b727ecbf23fa4
Bhargavi Kondareddy <bhargavi.kondareddy@citiustech.com>
2023-06-15 17:26:23 +0530

    done

---
+++ Sample_Project/src/main/java/com/bhargavi/Sample_Project/App.java
@@ -0,0 +1,13 @@
+package com.bhargavi.Sample_Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World i am learning maven" );
+    }
+}
